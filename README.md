418647dd87db0b72305da5a0f3a67b971eaa2d6f
Aaron Garry <apgarry99@gmail.com>
2026-04-16 22:07:45 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # is120-final-aaron-garry
 End of semester final project
+
+
+Watched youtube videos to learn how to do some scrolling animation. surprised to not have to use JS
+
+for dark mode learned how to use classlist.replace I just used that to change the classes from light ones to dark ones. 
+root is easier 
+
+USed "or' false when setting the local storage dark mode variable if it had never been set
---
+++ script.js
@@ -0,0 +1,32 @@
+// variables
+let light = document.querySelector("#light-button");
+let dark = document.querySelector("#dark-button");
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+let footer = document.querySelector("footer");
+let toggle_header = document.querySelector("#toggle-header");
+
+// local storage check for dark mode
+let dark_mode_status = JSON.parse(localStorage.getItem("darkMode")) || false;
+if (dark_mode_status) {
+  dark_mode();
+}
+
+// event listeners
+light.addEventListener("click", light_mode);
+dark.addEventListener("click", dark_mode);
+
+// functions
+function light_mode() {
+  body.classList.remove("dark-mode");
+  toggle_header.textContent = "Dark Mode";
+  dark_mode_status = false;
+  localStorage.setItem("darkMode", JSON.stringify(dark_mode_status));
+}
+
+function dark_mode() {
+  body.classList.add("dark-mode");
+  toggle_header.textContent = "Light Mode";
+  dark_mode_status = true;
+  localStorage.setItem("darkMode", JSON.stringify(dark_mode_status));
+}
